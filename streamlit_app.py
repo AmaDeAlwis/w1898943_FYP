481b88b3634293582fca0945b8d000d7f1744f98
AmaDeAlwis <108567211+AmaDeAlwis@users.noreply.github.com>
2025-04-28 13:06:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ st.set_page_config(page_title="Breast Cancer Survival UI", layout="wide")
 #Load model and scaler
 gcn_model = SurvivalGNN(
     in_channels=15,
-    hidden_channels=192,
+    hidden_channels=256,  
     out_channels_time=1,
     out_channels_event=1,
-    dropout_rate=0.16852060520867967
+    dropout_rate=0.10006882151829198  
 )
 gcn_model.load_state_dict(torch.load(".streamlit/gcn_model.pt", map_location=torch.device("cpu")))
 gcn_model.eval()

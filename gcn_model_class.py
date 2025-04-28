eb2c5f7bc87b7466134174700128ef97cfaa7971
AmaDeAlwis <108567211+AmaDeAlwis@users.noreply.github.com>
2025-04-28 11:59:34 +0530

    Update gcn_model_class.py

--- gcn_model_class.py
+++ gcn_model_class.py
@@ -4,22 +4,18 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 class SurvivalGNN(nn.Module):
-    def __init__(self, in_channels, out_channels_time, out_channels_event, dropout_rate=0.3):
+    def __init__(self, in_channels, hidden_channels, out_channels_time=1, out_channels_event=1, dropout_rate=0.3):
         super(SurvivalGNN, self).__init__()
-        self.conv1 = GCNConv(in_channels, 64)
-        self.conv2 = GCNConv(64, 64)
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.dropout = nn.Dropout(dropout_rate)
-        self.fc_time = nn.Linear(64, out_channels_time)
-        self.fc_event = nn.Linear(64, out_channels_event)
+        self.fc_time = nn.Linear(hidden_channels, out_channels_time)
+        self.fc_event = nn.Linear(hidden_channels, out_channels_event)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         x = F.relu(self.conv1(x, edge_index))
-        x = self.dropout(x)
-        x = F.relu(self.conv2(x, edge_index))
-        x = self.dropout(x)
-
+        x = self.dropout(F.relu(self.conv2(x, edge_index)))
         survival_time_output = self.fc_time(x)
         event_status_output = self.fc_event(x)
-
         return survival_time_output, event_status_output
